ec7fd2ed6da7ccc8e6f1a14e9776ddb63642898e
wxf <1337675840@qq.com>
2023-05-22 13:02:07 +0800

    修改为英文内容

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 var config = {
     /*在这里配置你的基本信息，所有数据以字符串形式给出*/
-    name: "卢世荣",
-    sex: "男",
-    age: "19",
-    phone: "19870887127",
-    email: "happysnaker@foxmail.com",
-    address: "现居浙江省义乌市",
-    qq: "1637318597",
-    log: "Happysnaker",
-    excpect_work: "Java/Go后端开发",
+    name: "伍孝飞(Wu,Xiaofei)",
+    // sex: "男",
+    // age: "22",
+    // phone: "19870887127",
+    email: "1337675840@qq.com",
+    address: "ShanghaiTech University",
+    // qq: "1637318597",
+    log: "Happycoder",
+    // excpect_work: "Java/Go后端开发",
 
 
     /*在这里配置首页的座右铭集合*/
@@ -31,47 +31,44 @@ var config = {
 
 
     /*在这里配置首页的见面信息，你可以内嵌HTML标签以调整格式*/
-    welcome: "青青子衿，悠悠我心<br>" +
-             "但为君故，沉吟至今<br>" +
-             "你好，我是卢世荣，南昌大学软件工程大二在读生<br>" +
-             "很高兴见到你!",
+    welcome: "The miracle appear in bad luck<br>",
 
 
     /*在这里配置关于我的信息，你可以内嵌HTML标签以调整格式*/
-    about: "<p>你好！我叫卢世荣，性别男，南昌大学软件学院大二在读。我期望的工作岗位是Go/Java后端开发。</p>" +
-        "<p>我有着较多的Java编程经验，计算机基础知识掌握扎实，能够在工作中很好的完成自己的任务。此外，我有着充满激情的工作态度，团队协同作战能力强，同时我也具备独立开发的能力，擅于发现并解决问题。我的执行力强、责任感高、集体荣誉感强、敢于担当，能够接受加班或出差等安排</p>" +
-        "<p>十分期待与您的联系!</p>",
-
+    about: "<p>I am Wu Xiaofei. I completed my undergraduate studies in Computer Science /and Technology at Northeast University, and pursued my graduate studies at ShanghaiTech University. My research focuses on computer vision, human behavior prediction, and interactive human-computer interaction.</p>" 
+        // "<p>我有着较多的Java编程经验，计算机基础知识掌握扎实，能够在工作中很好的完成自己的任务。此外，我有着充满激情的工作态度，团队协同作战能力强，同时我也具备独立开发的能力，擅于发现并解决问题。我的执行力强、责任感高、集体荣誉感强、敢于担当，能够接受加班或出差等安排</p>" +
+        // "<p>十分期待与您的联系!</p>",
+        ,
 
 
     /** 
     * 在这里配置你的技能点
     * ["技能点", 掌握程度, "技能条颜色"]
-    */  
-    skills: [
-        ["Java", 80, "red"],
-        ["GoLang", 77, "blue"],
-        ["SQL", 75, "#1abc9c"],
-        ["HTML5", 67, "rgba(0,0,0)"],
-        ["CSS3", 60, "yellow"],
-        ["JavaScript", 70, "pink"]
-    ],
+    */
+    // skills: [
+    //     ["Java", 80, "red"],
+    //     ["GoLang", 77, "blue"],
+    //     ["SQL", 75, "#1abc9c"],
+    //     ["HTML5", 67, "rgba(0,0,0)"],
+    //     ["CSS3", 60, "yellow"],
+    //     ["JavaScript", 70, "pink"]
+    // ],
 
 
-    /*这里填写你的技能描述，你可以内嵌HTML标签以调整格式*/
-    skills_description: "<ul>" +
-        "     <li>操作系统、计算机网络等编程基础知识良好。</li>" +
-        "     <li>熟练掌握Java基础。</li>" +
-        "     <li>熟悉JavaIO、多线程、集合等基础框架。</li>" +
-        "     <li>了解JVM原理。</li>" +
-        "     <li>熟悉Go语言开发基本知识。</li>" +
-        "     <li>熟悉SQL语句编写以及调优。</li>" +
-        "     <li>熟悉基本Linux命令操作。</li>" +
-        "     <li>熟悉Spring、ibatis、struts等框架的使用，了解其原理与机制。</li>" +
-        "     <li>熟悉缓存、消息等机制。</li>" +
-        "     <li>了解分布式系统的设计与应用。</li>" +
-        "     <li>熟悉HTML、CSS、JavaScript以及相应前端知识。</li>" +
-        " </ul>",
+    // /*这里填写你的技能描述，你可以内嵌HTML标签以调整格式*/
+    // skills_description: "<ul>" +
+    //     "     <li>操作系统、计算机网络等编程基础知识良好。</li>" +
+    //     "     <li>熟练掌握Java基础。</li>" +
+    //     "     <li>熟悉JavaIO、多线程、集合等基础框架。</li>" +
+    //     "     <li>了解JVM原理。</li>" +
+    //     "     <li>熟悉Go语言开发基本知识。</li>" +
+    //     "     <li>熟悉SQL语句编写以及调优。</li>" +
+    //     "     <li>熟悉基本Linux命令操作。</li>" +
+    //     "     <li>熟悉Spring、ibatis、struts等框架的使用，了解其原理与机制。</li>" +
+    //     "     <li>熟悉缓存、消息等机制。</li>" +
+    //     "     <li>了解分布式系统的设计与应用。</li>" +
+    //     "     <li>熟悉HTML、CSS、JavaScript以及相应前端知识。</li>" +
+    //     " </ul>",
 
 
     /**
@@ -81,9 +78,8 @@ var config = {
      * 通过查看实际效果以调整字题长度
      */
     portfolio: [
-        ["./images/pro-1.png", "http://1.15.234.109:8000/", "个人博客", "这里记录了我的Java后端学习笔记<br>持续更新"],
-        ["./images/pro-2.png", "https://github.com/happysnaker/Gobang", "智能人机对战五子棋", "采用C++编写的智能五子棋人机对战<br>2021/7/23"],
-        ["https://pic3.zhimg.com/80/v2-d9766956d5c85c2780e4c5008fd946ca_1440w.jpg", "https://github.com/happysnaker/StudentsManageSystem", "学生管理系统", "C语言+AVL树+多重双向表实现"]
+        ["./images/xgboost-predernn.png", "http://www.c-s-a.org.cn/html/2022/10/8731.html", "中文核心", "时序预测"],
+
     ],
 
 
@@ -92,22 +88,22 @@ var config = {
      * ["日期"， "工作"， "介绍"]
      * 你可以内嵌HTML标签以排版格式
      */
-    work: [
-        //如果您内有工作经历，您可以采取下列写法
-        // ["————————", "", "<p>暂无工作经历，期待您的联系。</p>"]
+    // work: [
+    //     //如果您内有工作经历，您可以采取下列写法
+    //     // ["————————", "", "<p>暂无工作经历，期待您的联系。</p>"]
 
-        ["2020/7/1 — 2021/8/10", "<br>阎王殿实习生",
-            "<p><strong>阎王殿研发部</strong></p>" +
-            "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>" +
-            "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>"
-        ],
+    //     ["2020/7/1 — 2021/8/10", "<br>阎王殿实习生",
+    //         "<p><strong>阎王殿研发部</strong></p>" +
+    //         "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>" +
+    //         "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>"
+    //     ],
 
-        ["2020/7/1 — 2021/8/10", "<br>阎王殿实习生",
-            "<p><strong>阎王殿研发部</strong></p>" +
-            "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>" +
-            "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>"
-        ]
-    ],
+    //     ["2020/7/1 — 2021/8/10", "<br>阎王殿实习生",
+    //         "<p><strong>阎王殿研发部</strong></p>" +
+    //         "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>" +
+    //         "<p>随着阴历7月15中元节的到来，阎王殿的任务愈发庞大，我以及我所在小组主要负责阎王谱后台部分，拟在解决千万访问并发问题，经过不械努力，使得产品稳定、高效的运行。</p>"
+    //     ]
+    // ],
 
 
     /**
@@ -116,10 +112,12 @@ var config = {
      * 建议填写您的校级及以上得奖经历或或其他证书
      */
     others: [
-        ["2021-04-28", "第十二届蓝桥杯大学生A组省三等奖", "大一下学期，我参与第十二届蓝桥杯大学生A组，然比赛一改以往暴力题，半数以上DP，仅取得省级三等奖。"],
-        ["2021-04-24", "第六届团队程序设计天梯赛个人国家三等奖", "大一下学期，我通过面向全年级的选拔，获得入队名额，在个人赛中获得全国三等奖。"],
-        ["2021-04-24", "第六届团队程序设计天梯赛团体国家二等奖", "大一下学期，我通过面向全年级的选拔，获得入队名额，跟随团队取得团体国家二等奖的成绩。"],
-        ["2020-11-14", "2020级南昌大学程序设计正式赛三等奖", "大一上学期，我参与校举办的面向全校程序设计大赛并获得三等奖，"]
+        ["2023", "上海科技大学","Admitted to ShanghaiTech University"],
+        ["2022", "CCPC区域赛 银奖","CCPC Regional Competition Silver Award"],
+        ["2022", "robocom机器人开发者大赛国家二等奖", "The National Second Prize of Robocom Robot Developer Contest"],
+        ["2021", "程序设计天梯赛个人国家三等奖", "National Third Prize in Program Design Ladder Competition"],
+        ["2020", "辽宁省程序设计竞赛金奖", "Gold Award of Liaoning Province Programming Competition"],
+        ["2019", "东北大学", "Admitted to Northeastern University"],
     ],
 
 
@@ -132,11 +130,11 @@ var config = {
      * 建议您放置数量 <= 5
      */
     icon: [
-        ["./svg/LeetCode.svg", "https://leetcode-cn.com/u/happysnaker/", "我的力扣主页"],
-        ["./svg/github.svg", "https://github.com/happysnaker", "我的GitHub主页"],
-        ["./svg/博客.svg", "http://1.15.234.109:8000", "我的个人博客"],
-        ["./svg/掘金.svg", "https://juejin.cn/user/3853167638625000", "我的掘金主页"],
-        ["./svg/知乎.svg", "https://www.zhihu.com/people/tian-xia-you-dao-81", "我的知乎主页"]
+        // ["./svg/LeetCode.svg", "https://leetcode-cn.com/u/happysnaker/", "我的力扣主页"],
+        ["./svg/github.svg", "https://github.com/wxf-0415", "我的GitHub主页"],
+        // ["./svg/博客.svg", "http://1.15.234.109:8000", "我的个人博客"],
+        // ["./svg/掘金.svg", "https://juejin.cn/user/3853167638625000", "我的掘金主页"],
+        ["./svg/知乎.svg", "https://www.zhihu.com/people/79-24-97-64", "我的知乎主页"]
     ],
 
 
